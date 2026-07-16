33b9fd2dc02988c5ddf2f456ec049393f816d445
Dill312 <allyrose312@gmail.com>
2026-07-16 14:00:02 -0500

    Add files via upload

---
+++ watchlist.py
@@ -0,0 +1,127 @@
+watchlist = [
+    {
+        "stock": "AAPL",
+        "company": "Apple",
+        "threshold": 5
+    },
+    {
+        "stock": "MSFT",
+        "company": "Microsoft",
+        "threshold": 5
+    },
+    {
+        "stock": "GOOGL",
+        "company": "Alphabet",
+        "threshold": 5
+    },
+    {
+        "stock": "AMZN",
+        "company": "Amazon",
+        "threshold": 6
+    },
+    {
+        "stock": "META",
+        "company": "Meta Platforms",
+        "threshold": 7
+    },
+    {
+        "stock": "NVDA",
+        "company": "NVIDIA",
+        "threshold": 8
+    },
+    {
+        "stock": "TSLA",
+        "company": "Tesla Inc",
+        "threshold": 10
+    },
+    {
+        "stock": "AMD",
+        "company": "Advanced Micro Devices",
+        "threshold": 8
+    },
+    {
+        "stock": "AVGO",
+        "company": "Broadcom",
+        "threshold": 7
+    },
+    {
+        "stock": "TSM",
+        "company": "Taiwan Semiconductor Manufacturing Company",
+        "threshold": 6
+    },
+    {
+        "stock": "COST",
+        "company": "Costco Wholesale",
+        "threshold": 5
+    },
+    {
+        "stock": "WMT",
+        "company": "Walmart",
+        "threshold": 4
+    },
+    {
+        "stock": "HD",
+        "company": "The Home Depot",
+        "threshold": 5
+    },
+    {
+        "stock": "NKE",
+        "company": "Nike",
+        "threshold": 6
+    },
+    {
+        "stock": "JPM",
+        "company": "JPMorgan Chase",
+        "threshold": 5
+    },
+    {
+        "stock": "V",
+        "company": "Visa",
+        "threshold": 5
+    },
+    {
+        "stock": "MA",
+        "company": "Mastercard",
+        "threshold": 5
+    },
+    {
+        "stock": "LLY",
+        "company": "Eli Lilly",
+        "threshold": 6
+    },
+    {
+        "stock": "ABBV",
+        "company": "AbbVie",
+        "threshold": 5
+    },
+    {
+        "stock": "JNJ",
+        "company": "Johnson & Johnson",
+        "threshold": 5
+    },
+    {
+        "stock": "XOM",
+        "company": "Exxon Mobil",
+        "threshold": 5
+    },
+    {
+        "stock": "CVX",
+        "company": "Chevron",
+        "threshold": 5
+    },
+    {
+        "stock": "CAT",
+        "company": "Caterpillar",
+        "threshold": 6
+    },
+    {
+        "stock": "PLTR",
+        "company": "Palantir Technologies",
+        "threshold": 10
+    },
+    {
+        "stock": "COIN",
+        "company": "Coinbase Global",
+        "threshold": 12
+    },
+]
